43965f20a43c8eef3efed9e1738012a7f213146f
AiaRup <aiale88@gmail.com>
2019-04-08 08:47:59 +0200

    game logic ready

--- js/main.js
+++ js/main.js
@@ -1,11 +1,47 @@
 'use strict';
 
-console.log('hello');
+// dom elements to work with
+const input = document.querySelector('#number');
+const numGuessesText = document.querySelector('.guess');
+const btn = document.querySelector('.submitBtn');
+const hintText = document.querySelector('.hint');
 
-// Función que genera un número aleatorio hasta un máximo dado
+let winningNumber = 0;
+let numberOfGuesses = 0;
+
+// Function to generate a number between 1 to 100
 const getRandomNumber = max => {
   return Math.ceil(Math.random() * max);
 };
 
+const checkGuess = event => {
+  event.preventDefault();
+  // take input value
+  const value = parseInt(input.value);
+  console.log(`The user entered the number: ${value}`);
+  // check if the number is correct
+  if (winningNumber === value) {
+    hintText.innerHTML = '¡HAS GANADO, CAMPEONA!';
+    // numberOfGuesses++;
+  } else if (value > winningNumber) {
+    hintText.innerHTML = 'demasiado alto';
+    // numberOfGuesses++;
+  } else {
+    hintText.innerHTML = 'demasiado bajo';
+  }
+  // add 1 to guesses
+  numberOfGuesses++;
+  numGuessesText.innerHTML = numberOfGuesses;
+};
+
 // generate a number between 1 and 100;
-getRandomNumber(100);
+const startGame = () => {
+  winningNumber = getRandomNumber(100);
+  console.log(`winning number is: ${winningNumber}`);
+  numberOfGuesses = 0;
+};
+
+startGame();
+
+//event listener
+btn.addEventListener('click', checkGuess);
