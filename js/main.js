a440473e2eb6ed60e1ecfef7466a98e95b3ddf83
AiaRup <aiale88@gmail.com>
2019-04-10 18:42:49 +0200

    add enter function, check empty value or in range, check winning state

--- js/main.js
+++ js/main.js
@@ -10,28 +10,56 @@ const resetButton = document.querySelector('.resetBtn');
 // variables to work with
 let winningNumber = 0;
 let numberOfGuesses = 0;
+let wonGame = false;
 
 // Function to generate a random number between 1 to max
 const getRandomNumber = max => {
   return Math.ceil(Math.random() * max);
 };
 
-const checkGuess = event => {
-  event.preventDefault();
-  // take input value
-  const value = parseInt(input.value);
-  console.log(`The user entered the number: ${value}`);
-  // check if the number is correct, too height or too low
+// if the input is empty or the user entered a number bigger than 100 or a negative number
+const notInRange = value => {
+  if (!value) {
+    hintText.innerHTML = 'Try guess the winning number by writing a number in the field.';
+    return false;
+  }
+  if (value <= 0 || value > 100) {
+    hintText.innerHTML = 'The winning number is between 1 and 100, try guess in that range...';
+    numberOfGuesses++;
+    numGuessesText.innerHTML = numberOfGuesses;
+    return false;
+  }
+  return true;
+};
+
+// if in range check if the number is correct, too height or too low
+const checkWinning = value => {
   if (winningNumber === value) {
+    wonGame = true;
     hintText.innerHTML = '¡HAS GANADO, CAMPEONA!';
+    resetButton.classList.toggle('hidden');
+    btn.classList.toggle('hidden');
   } else if (value > winningNumber) {
     hintText.innerHTML = 'demasiado alto';
   } else {
     hintText.innerHTML = 'demasiado bajo';
   }
-  // add 1 to num of guesses
   numberOfGuesses++;
   numGuessesText.innerHTML = numberOfGuesses;
+};
+
+const checkGuess = event => {
+  event.preventDefault();
+  if (!wonGame) {
+    const value = parseInt(input.value);
+    console.log(`The user entered the number: ${value}`);
+    const inRange = notInRange(value);
+    if (inRange) {
+      checkWinning(value);
+    }
+  } else {
+    hintText.innerHTML = 'You already WON! click Try Again if you want to reset the game.';
+  }
 };
 
 // start game and generate a number between 1 and 100;
@@ -39,17 +67,28 @@ const startGame = () => {
   winningNumber = getRandomNumber(100);
   console.log(`winning number is: ${winningNumber}`);
   numberOfGuesses = 0;
+  numGuessesText.innerHTML = numberOfGuesses;
 };
 
+// reset game after winning
 const resetGame = () => {
   startGame();
-  numGuessesText.innerHTML = numberOfGuesses;
   hintText.innerHTML = 'Escribe un número y dale a <span class="italic">prueba</span>';
   input.value = '';
+  btn.classList.toggle('hidden');
+  resetButton.classList.toggle('hidden');
+  wonGame = false;
+};
+
+const checkEnterPress = event => {
+  if (event.keyCode === 13) {
+    checkGuess(event);
+  }
 };
 
 startGame();
 
-// event listener on the button
+// event listeners on the buttons and window
+window.addEventListener('keypress', checkEnterPress);
 btn.addEventListener('click', checkGuess);
 resetButton.addEventListener('click', resetGame);
